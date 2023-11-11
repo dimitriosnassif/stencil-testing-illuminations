a3ae6c80baea7ad0d1e165988d602e1a387bcfe5
dimii <dimitrios.nassif2002@gmail.com>
2023-11-11 14:51:56 -0500

    feat: init gouraud shaders

--- src/shaders/gouraud.fs.glsl
+++ src/shaders/gouraud.fs.glsl
@@ -12,8 +12,5 @@ out vec4 FragColor;
 
 void main()
 {
-    vec4 textureColor = texture(textureSampler, attribIn.texCoords);
-    vec3 finalColor = attribIn.color * textureColor.rgb;
-
-    FragColor = vec4(finalColor, 1.0);
+    
 }
--- src/shaders/gouraud.vs.glsl
+++ src/shaders/gouraud.vs.glsl
@@ -47,62 +47,15 @@ layout (std140) uniform LightingBlock
 
 float computeSpot(in vec3 spotDir, in vec3 lightDir, in vec3 normal)
 {
-    float cosSpotAngle = dot(-lightDir, normalize(spotDir));
-    if (cosSpotAngle > cos(spotOpeningAngle * 0.5))
-    {
-        return pow(cosSpotAngle, spotExponent);
-    }
-    else
-    {
-        return 0.0;
-    }
+    
 }
 
 vec3 computeLight(in int lightIndex, in vec3 normal, in vec3 lightDir, in vec3 obsPos)
 {
-    vec3 N = normalize(normal);
-    vec3 L = normalize(lights[lightIndex].position - vertexPos);
-    vec3 V = normalize(-vertexPos);
-    vec3 R = reflect(-L, N);
-    vec3 H = normalize(L + V);
-
-    vec3 ambient = lights[lightIndex].ambient * mat.ambient;
-
-    float diff = max(dot(N, L), 0.0);
-    vec3 diffuse = lights[lightIndex].diffuse * (diff * mat.diffuse);
-
-    float spec = 0.0;
-    if (useBlinn)
-    {
-        spec = pow(max(dot(N, H), 0.0), mat.shininess);
-    }
-    else
-    {
-        spec = pow(max(dot(R, V), 0.0), mat.shininess);
-    }
-    vec3 specular = lights[lightIndex].specular * (spec * mat.specular);
-
-    float spotEffect = useSpotlight ? computeSpot(lights[lightIndex].spotDirection, L, N) : 1.0;
-
-    vec3 lightColor = (ambient + (diffuse + specular) * spotEffect);
-
-    return lightColor;
+    
 }
 
 void main()
 {
-    vec4 viewPos = modelView * vec4(position, 1.0);
-    vec3 norm = normalize(normalMatrix * normal);
-
-    vec3 lightAccumulation = mat.emission + lightModelAmbient * mat.ambient;
-
-    for (int i = 0; i < 3; ++i)
-    {
-        lightAccumulation += computeLight(i, norm, viewPos.xyz);
-    }
-
-    attribOut.texCoords = texCoords;
-    attribOut.color = lightAccumulation;
-
-    gl_Position = mvp * vec4(position, 1.0);
+    
 }
